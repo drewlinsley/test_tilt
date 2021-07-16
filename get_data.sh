d6122e5578f48d8071663b959ed4d155d45d4432
c-rbp <crbpproject@gmail.com>
2021-07-16 14:42:00 -0400

    Tilt illusion eval code.

---
+++ get_data.sh
@@ -0,0 +1,10 @@
+scp -r drew@x9.clps.brown.edu:/media/data_cifs/cluster_projects/refactor_gammanet/gratings/BSDS_vgg_gratings_horizontal_bigger_gratings_test_2019_11_08_09_28_20_400607.npz gratings/
+
+scp -r drew@x9.clps.brown.edu:/media/data_cifs/cluster_projects/refactor_gammanet/gratings/BSDS_vgg_gratings_simple_no_nonnegative_gratings_test_2019_11_08_09_31_49_221740.npz gratings/
+
+scp -r drew@x9.clps.brown.edu:/media/data_cifs/cluster_projects/refactor_gammanet/gratings/BSDS_vgg_gratings_simple_no_additive_gratings_test_2019_11_08_09_34_28_318406.npz gratings/
+
+scp -r drew@x9.clps.brown.edu:/media/data_cifs/cluster_projects/refactor_gammanet/gratings/BSDS_vgg_gratings_simple_no_additive_no_multiplicative_gratings_test_2019_11_08_09_37_09_211667.npz gratings/
+
+scp -r drew@x9.clps.brown.edu:/media/data_cifs/cluster_projects/refactor_gammanet/gratings/BSDS_vgg_gratings_simple_no_multiplicative_gratings_test_2019_11_08_09_57_28_269615.npz gratings/
+
